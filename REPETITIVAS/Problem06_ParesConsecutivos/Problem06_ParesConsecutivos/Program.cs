5bff8388073fe9c34c8b66f61ab4621f873cfff0
Harddolf <103981515+Harddolf@users.noreply.github.com>
2023-01-29 08:21:39 -0300

    Commit to main

---
+++ REPETITIVAS/Problem06_ParesConsecutivos/Problem06_ParesConsecutivos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Program
+{
+    class ParesConsecutivos
+    {
+        static void Main(string[] args)
+        {
+            int x, soma;
+
+            Console.WriteLine("Digite um numero inteiro: ");
+            x = int.Parse(Console.ReadLine());
+
+            while(x != 0)
+            {
+                if(x % 2 != 0)
+                {
+                    x++;
+                }
+                soma = (x * 5) + 20;
+                Console.WriteLine($"Soma = {soma}");
+                Console.WriteLine("Digite um numero inteiro: ");
+                x = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
