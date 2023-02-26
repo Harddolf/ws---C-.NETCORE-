f545308ce6fc243cbed699d68f9803ccc3eff7a8
Harddolf <103981515+Harddolf@users.noreply.github.com>
2023-02-26 00:47:07 -0300

    Commit

---
+++ Curso/ExsPropostos203/ExsPropostos203/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExsPropostos203
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            string[] stg = Console.ReadLine().Split(" ");
+            int valor01 = int.Parse(stg[0]); 
+            int valor02 = int.Parse(stg[1]);
+            if(valor01 % 2 == 0 && valor02 % 2 == 0)
+            {
+                Console.WriteLine("São multiplos");
+            }
+            else
+                Console.WriteLine("Não são multiplos");
+        }
+    }
+}
