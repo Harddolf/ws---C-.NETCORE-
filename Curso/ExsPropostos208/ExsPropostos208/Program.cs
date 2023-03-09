62b67be35a4530f801bec68168d145a20df4ae6f
Harddolf <103981515+Harddolf@users.noreply.github.com>
2023-03-09 19:05:10 -0300

    Commit.

---
+++ Curso/ExsPropostos208/ExsPropostos208/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExsPropostos208
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double renda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double imposto = 0;
+
+            if (renda <= 2000.00)
+            {
+                Console.WriteLine("Insento.");
+
+            }
+
+            else if (renda <= 3000.00)
+            {
+                imposto = (renda - 2000) * 0.08;
+                Console.WriteLine("R$: " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            else if (renda <= 4500.00)
+            {
+                imposto = (renda - 3000) * 0.18 + 1000 * 0.08;
+                Console.WriteLine("R$: " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            else if (renda > 4500)
+            {
+                imposto = (renda - 4500) * 0.28 + 1500 * 0.18 + 1000 * 0.08;
+                Console.WriteLine("R$: " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
